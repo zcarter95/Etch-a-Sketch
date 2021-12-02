78e6b2b390da892584fdb72d227dd21e8eaa29c5
Zach Carter <carter.peter.zach1@gmail.com>
2021-12-02 14:57:42 -0800

    different grid sizes implemented and slider resets grid

--- script.js
+++ script.js
@@ -1,29 +1,71 @@
 var container = document.querySelector('div');
+const button = document.querySelector('button');
 function createDiv(){
     var div = document.createElement('div');
     div.classList.add('item');
     return div;
 }
-
 var divs = []
 
-for(var i = 0; i < 16; i++){
-    divs[i] = createDiv();
-}
+var range = document.querySelector('input');
+var output = document.getElementById("output");
+output.textContent = range.value;
+var screenSize = range.value;
+screenSize = screenSize * screenSize;
 
-for (var i = 0; i < divs.length; i++){
-    container.appendChild(divs[i]);
-}
-
-items = document.querySelectorAll('.item');
-console.log(items);
-
-items.forEach((item) => {
-    console.log(item);
-    item.addEventListener('mouseenter', () => {
-        item.classList.add('hover');
+function drawPixels (screenSize){
+    for(var i = 0; i < (screenSize); i++){
+        divs[i] = createDiv();
+    }
+    
+    for (var i = 0; i < divs.length; i++){
+        container.appendChild(divs[i]);
+    }
+    
+    function determinePixelSize (number){
+        var pixelSize = 250000 / number;
+        var dimensions = Math.sqrt(pixelSize);
+        var dimensionsFormatted = dimensions + 'px';
+        return dimensionsFormatted;
+    }
+    
+    items = document.querySelectorAll('.item');
+    
+    items.forEach((item) => {
+        item.style.width = determinePixelSize(screenSize);
+        item.style.height = determinePixelSize(screenSize);
+        item.addEventListener('mouseenter', () => {
+            item.classList.add('hover');
+        })
     })
-})
+    
+    button.addEventListener('click', () => {
+        items.forEach((item) => {
+            item.classList.remove('hover');
+        })
+    })
+}
+
+function removeChildren () {
+    const divs = document.getElementsByClassName('item');
+    while(divs.length > 0){
+        console.log(divs[0]);
+        divs[0].parentNode.removeChild(divs[0]);
+    }
+}
+
+drawPixels(screenSize);
+range.oninput = function() {
+    output.textContent = this.value;
+    removeChildren();
+    drawPixels(screenSize);
+}
+
+
+
+
+
+
 
 
 
