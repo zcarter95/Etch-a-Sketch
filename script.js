e230305b759a058fa6c285f9f2f86294322a3566
Zach Carter <carter.peter.zach1@gmail.com>
2021-12-02 12:13:30 -0800

    completed inital functionality of etch-a-sketch

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const container = document.querySelector('.container');
-
-function addDivs (){
-    let div = document.createElement('div');
-    container.appendChild(div);
+var container = document.querySelector('div');
+function createDiv(){
+    var div = document.createElement('div');
+    div.classList.add('item');
     return div;
 }
+
+var divs = []
+
+for(var i = 0; i < 16; i++){
+    divs[i] = createDiv();
+}
+
+for (var i = 0; i < divs.length; i++){
+    container.appendChild(divs[i]);
+}
+
+items = document.querySelectorAll('.item');
+console.log(items);
+
+items.forEach((item) => {
+    console.log(item);
+    item.addEventListener('mouseenter', () => {
+        item.classList.add('hover');
+    })
+})
+
+
+
+
+
+
 
